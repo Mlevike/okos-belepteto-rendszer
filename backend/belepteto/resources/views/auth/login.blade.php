46e159bab987532db4938c48e4588696846dc6d1
Márton Levente <38218622+Mlevike@users.noreply.github.com>
2023-04-17 15:33:35 +0200

    Login error message added

--- backend/belepteto/resources/views/auth/login.blade.php
+++ backend/belepteto/resources/views/auth/login.blade.php
@@ -36,20 +36,25 @@
             border-top-left-radius: 0;
             border-top-right-radius: 0;
         }
+        .login-errors{
+            padding: 5px;
+            margin-top: 10px;
+        }
 
     </style>
 </head>
 <body class="text-center">
 <main class="form-signin w-50 m-auto">
-    <form>
+    <form action="" method="post">
+        @csrf
         <h1 class="h3 mb-3 fw-normal">{{ __('auth.please_login') }}</h1>
 
         <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email">
             <label for="floatingInput">{{ __('auth.email') }}</label>
         </div>
         <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="{{ __('auth.password') }}" name="password">
             <label for="floatingPassword">{{ __('auth.password') }}</label>
         </div>
 
@@ -59,6 +64,13 @@
             </label>
         </div>
         <button class="w-100 btn btn-lg btn-primary" type="submit">{{ __('auth.login') }}</button>
+        @if ($errors->any())
+            <div class="login-errors alert alert-danger">
+                    @foreach ($errors->all() as $error)
+                        <p>{{ $error }}</p>
+                    @endforeach
+            </div>
+        @endif
     </form>
 </main>
 </body>
