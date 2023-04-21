4cb1617d1639ec2530f7315d5ecd8655e78a7231
Mlevike <martonlevi399@gmail.com>
2023-04-21 13:42:20 +0100

    Argon2 hash added to the project

--- raspberry/hardwareLayer.py
+++ raspberry/hardwareLayer.py
@@ -10,7 +10,7 @@ import time
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import requests
-import bcrypt
+import argon2
 
 #internalCardDetected = False #Létrehozunk egy globális változót a belső kártyaérintés érzékelésére
 
