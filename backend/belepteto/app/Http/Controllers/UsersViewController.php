c72c168c0ef350a56a8417abf08cac0cc5b3abec
Márton Levente <38218622+Mlevike@users.noreply.github.com>
2023-04-29 12:03:13 +0200

    Remove unecessary slash

--- backend/belepteto/app/Http/Controllers/UsersViewController.php
+++ backend/belepteto/app/Http/Controllers/UsersViewController.php
@@ -36,6 +36,23 @@ class UsersViewController extends Controller
          //return view('users.add', ['user' => User::all(), 'error' => "Külső"]);
      }
 
+     public function edit(Request $request){
+
+         if ($request->isMethod('GET')){
+             return view('users.edit', ['user' => User::all(), 'errors' => "", "userId" => $id]);
+         }
+         if ($request->isMethod('POST'))
+         {
+             if(($request->filled('name')) and ($request->filled('email')) and  ($request->filled('password'))){
+                 User::create(['name'=> $request->name, 'email'=> $request->email, 'email_verified_at'=> now(), 'password'=> Hash::make($request->password, ['memory' => 1024, 'time' => 2, 'threads' => 2,]), 'picture'=>'', 'code'=>Hash::make('1111', ['memory' => 1024, 'time' => 2, 'threads' => 2,]), 'fingerprint'=>'', 'language'=>'en', 'profile'=>'Kártya2', 'isAdmin'=> false, 'isWebEnabled'=> false, 'isEntryEnabled'=> true, 'isEmployee'=> false, 'cardId' =>'724b41f
+']);             return redirect('/users')->with('status', 'Felhasználó törölve!');
+             }else{
+                 return view('users.edit', ['user' => User::all(), 'errors' => "A csillagal jelölt mezők kitöltése kötelező!"]);
+             }
+
+         }
+     }
+
     public function delete(Request $request){
         User::find($request->id)->delete();
         return redirect('/users')->with('status', 'Felhasználó törölve!');
--- backend/belepteto/routes/web.php
+++ backend/belepteto/routes/web.php
@@ -19,27 +19,32 @@ use App\Http\Resources\ValidateResource;
 //Itt találhatóak a különböző nézetekhez tartozó útvonal definiciók
 
 //Vezérlőpulthoz tartozó útvonal
-Route::get('/', function () {
+Route::get('', function () {
     return view('dashboard');
 })->middleware('auth');
 
 //A logok oldalhoz tartozó útvonal
-Route::get('/logs', function () {
+Route::get('logs', function () {
     return view('logs');
 })->middleware('auth');
 
 //A felhasználók oldalhoz tartozó útvonalak
 
-Route::get('/users', 'App\Http\Controllers\UsersViewController@index')->middleware('auth');
+Route::get('users', 'App\Http\Controllers\UsersViewController@index')->middleware('auth');
 //Route::match(['get', 'post'], '/users/add', 'App\Http\Controllers\UsersViewController@add')->middleware('auth');
-Route::get('/users/add', 'App\Http\Controllers\UsersViewController@add')->middleware('auth'); //Ezt majd később lehet egy sorba is írni!
-Route::post('/users/add', 'App\Http\Controllers\UsersViewController@add')->middleware('auth');
-Route::match(['get', 'post'],'/users/delete/', 'App\Http\Controllers\UsersViewController@delete')->middleware('auth');
+
+Route::get('users/add', 'App\Http\Controllers\UsersViewController@add')->middleware('auth'); //Ezt majd később lehet egy sorba is írni!
+Route::post('users/add', 'App\Http\Controllers\UsersViewController@add')->middleware('auth');
+
+Route::get('users/edit/{id}', 'App\Http\Controllers\UsersViewController@edit')->middleware('auth'); //Ezt majd később lehet egy sorba is írni!
+Route::post('users/edit/{id}', 'App\Http\Controllers\UsersViewController@edit')->middleware('auth');
+
+Route::match(['get', 'post'],'users/delete/', 'App\Http\Controllers\UsersViewController@delete')->middleware('auth');
 
 
 //A kártya validációhoz tartozó útvonal
 //Ez még csak ideiglenes, a végleges változatban majd az adatbázisból kéri le az információkat
-Route::get('/validate/{uid}', function() {
+Route::get('validate/{uid}', function() {
     //Az uid beolvasása a kérésből
     $uid = Route::input('uid');
     //A megfelelő cardID-val rendelkező user kiválasztása
