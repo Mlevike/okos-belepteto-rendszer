2137fd9b470a0b9be39467c5d5a847b9f9641066
Márton Levente <38218622+Mlevike@users.noreply.github.com>
2023-05-05 12:39:07 +0200

    Added isHere column to users table

--- backend/belepteto/app/Console/Commands/CreateFirstUser.php
+++ backend/belepteto/app/Console/Commands/CreateFirstUser.php
@@ -27,7 +27,7 @@ class CreateFirstUser extends Command
      */
     public function handle(): void
     {
-        User::create(['name'=>$this->argument('name'), 'email'=>$this->argument('email'), 'email_verified_at'=> now(), 'password'=> Hash::make($this->argument('password'), ['memory' => 1024, 'time' => 2, 'threads' => 2,]), 'picture'=>$this->argument('picture') != null ? $this->argument('picture') : '', 'code'=> $this->argument('code') != null ? Hash::make($this->argument('code'), ['memory' => 1024, 'time' => 2, 'threads' => 2,]) : '' , 'fingerprint'=> $this->argument('fingerprint') != null ? $this->argument('fingerprint') : '', 'language'=>$this->argument('language'), 'profile'=>$this->argument('profile'), 'isAdmin'=> true, 'isWebEnabled'=> true, 'isEntryEnabled'=> true, 'isEmployee'=> true, 'cardId' => $this->argument('cardId') != null ? $this->argument('cardId'): '']); //A felhasználó létrehozása
+        User::create(['name'=>$this->argument('name'), 'email'=>$this->argument('email'), 'email_verified_at'=> now(), 'password'=> Hash::make($this->argument('password'), ['memory' => 1024, 'time' => 2, 'threads' => 2,]), 'picture'=>$this->argument('picture') != null ? $this->argument('picture') : '', 'code'=> $this->argument('code') != null ? Hash::make($this->argument('code'), ['memory' => 1024, 'time' => 2, 'threads' => 2,]) : '' , 'fingerprint'=> $this->argument('fingerprint') != null ? $this->argument('fingerprint') : '', 'language'=>$this->argument('language'), 'profile'=>$this->argument('profile'), 'isAdmin'=> true, 'isWebEnabled'=> true, 'isEntryEnabled'=> true, 'isEmployee'=> true, 'cardId' => $this->argument('cardId') != null ? $this->argument('cardId'): '', 'isHere'=>false]); //A felhasználó létrehozása
         $this->info("Kész!");
     }
 }
