d24ec0239eee67efe52189e80e1b616061dbe570
Márton Levente <38218622+Mlevike@users.noreply.github.com>
2023-05-05 09:42:55 +0200

    Update show.blade.php

--- backend/belepteto/resources/views/users/show.blade.php
+++ backend/belepteto/resources/views/users/show.blade.php
@@ -7,8 +7,19 @@
 <body>
 @include('header')
 <h1>{{ $user->name }}</h1>
-<div>
-    <img alt="{{ __('site.picture') }}" src="{{$user->picture}}"/>
+<div class="container">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    <div class="col">
+    @if($user->picture == "")
+        <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+        </svg>
+    @else
+        <img alt="{{ __('site.picture') }}" src="{{$user->picture}}"/>
+    @endif
+    </div>
+    <div class="col">
     <p>{{ __('site.language') }}: {{$user->language}}</p>
     <p>{{ __('site.profile') }}: {{$user->profile}}</p>
     <p>{{ __('site.isAdmin') }}:
@@ -41,7 +52,9 @@
     </p>
     <p>{{ __('auth.email') }}: {{$user->email}}</p>
     <p>{{ __('site.cardId') }}: {{$user->cardId}}</p>
-    <a type="button" class="btn btn-danger" href="{{ route('users') }}" role="button">{{ __('site.cancel') }}</a>
 </div>
+</div>
+</div>
+<a type="button" class="btn btn-danger" href="{{ route('users') }}" role="button">{{ __('site.cancel') }}</a>
 </body>
 </html>
