e23bfb2bd496c83a72e75747b979baf7431dfcea
Márton Levente <38218622+Mlevike@users.noreply.github.com>
2023-05-31 12:53:49 +0200

    Web access permission now works

--- backend/belepteto/app/Providers/FortifyServiceProvider.php
+++ backend/belepteto/app/Providers/FortifyServiceProvider.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
 use Laravel\Fortify\Contracts\LogoutResponse;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -40,6 +42,15 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+        Fortify::authenticateUsing(function (Request $request) { //Ez felel a hasWebAccess működéséért, illetve az Authetikációért
+            $user = User::where('email', $request->email)->first();
+
+            if ($user &&
+                Hash::check($request->password, $user->password) && $user->isWebEnabled) {
+                return $user;
+            }
+        });
 
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
