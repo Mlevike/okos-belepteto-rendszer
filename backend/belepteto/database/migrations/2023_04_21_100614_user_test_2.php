ffe606cb6a822c05fc94e3ed3e5dcb122501d1c6
Márton Levente <38218622+Mlevike@users.noreply.github.com>
2023-05-31 16:49:59 +0200

    Test

--- backend/belepteto/database/migrations/2023_04_21_100614_user_test_2.php
+++ backend/belepteto/database/migrations/2023_04_21_100614_user_test_2.php
@@ -29,6 +29,68 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        if (Schema::hasColumn('users', 'picture'))
+        {
+            Schema::table('users', function (Blueprint $table)
+            {
+                $table->dropColumn('picture');
+            });
+        }
+        if (Schema::hasColumn('users', 'code'))
+        {
+            Schema::table('users', function (Blueprint $table)
+            {
+                $table->dropColumn('code');
+            });
+        }
+        if (Schema::hasColumn('users', 'fingerprint'))
+        {
+            Schema::table('users', function (Blueprint $table)
+            {
+                $table->dropColumn('fingerprint');
+            });
+        }
+        if (Schema::hasColumn('users', 'language'))
+        {
+            Schema::table('users', function (Blueprint $table)
+            {
+                $table->dropColumn('language');
+            });
+        }
+        if (Schema::hasColumn('users', 'profile'))
+        {
+            Schema::table('users', function (Blueprint $table)
+            {
+                $table->dropColumn('profile');
+            });
+        }
+        if (Schema::hasColumn('users', 'isAdmin'))
+        {
+            Schema::table('users', function (Blueprint $table)
+            {
+                $table->dropColumn('isAdmin');
+            });
+        }
+        if (Schema::hasColumn('users', 'isWebEnabled'))
+        {
+            Schema::table('users', function (Blueprint $table)
+            {
+                $table->dropColumn('isWebEnabled');
+            });
+        }
+        if (Schema::hasColumn('users', 'isEntryEnabled'))
+        {
+            Schema::table('users', function (Blueprint $table)
+            {
+                $table->dropColumn('isEntryEnabled');
+            });
+        }
+        if (Schema::hasColumn('users', 'isEmployee'))
+        {
+            Schema::table('users', function (Blueprint $table)
+            {
+                $table->dropColumn('isEmployee');
+            });
+        }
     }
 };
--- backend/belepteto/database/migrations/2023_04_21_105728_users_add_cardid.php
+++ backend/belepteto/database/migrations/2023_04_21_105728_users_add_cardid.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function ($table) {
-            $table->string('cardId');
+            $table->string('cardId')->nullable();
         });
     }
 
