de961b5af82f45092e26b23a280901f1969f80c5
Márton Levente <38218622+Mlevike@users.noreply.github.com>
2023-09-06 22:33:02 +0200

    Fingerprint reading implementation

--- arduino/belepteto/belepteto.ino
+++ arduino/belepteto/belepteto.ino
@@ -81,14 +81,14 @@ void LongBeep(){
   }
 }
 
-void CustomBeep(int freq, int delayedTime){
+/*void CustomBeep(int freq, int delayedTime){
   if(!MUTED){
   pinMode(BUZZER_PIN, OUTPUT);
   tone(BUZZER_PIN, freq);
   delay(delayedTime);
   noTone(BUZZER_PIN);
   }
-}
+}*/
 
 void LcdGoto(int row, int column){ //Az LCD-n adott pozicióra ugrásért felelő metódus
   if(row == 0){
@@ -110,144 +110,34 @@ void SoftwareReset(){ //Az eszköz szoftverből való újraindítására szolgá
   asm volatile ("jmp 0");
 }
 
-int EnrollFingerprint(){
-   int p = -1;
-  Serial.print("Waiting for valid finger to enroll as #"); Serial.println(id);
-  while (p != FINGERPRINT_OK) {
-    p = finger.getImage();
-    switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image taken");
-      break;
-    case FINGERPRINT_NOFINGER:
-      Serial.println(".");
-      break;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      break;
-    case FINGERPRINT_IMAGEFAIL:
-      Serial.println("Imaging error");
-      break;
-    default:
-      Serial.println("Unknown error");
-      break;
-    }
-  }
+int FingerprintGetImage(){
+  int p = -1;
+   while (p != FINGERPRINT_OK) { //Megróbálkozunk az ujjlenyomat felvétellel
+    p = finger.getImage(); //Itt nincs hibakijelzés
+   }
+   return p;
+}
 
-  // OK success!
+int FingerprintGenerateTemplate(int nr){
+  return finger.image2Tz(nr);
+  
+}
 
-  p = finger.image2Tz(1);
-  switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image converted");
-      break;
-    case FINGERPRINT_IMAGEMESS:
-      Serial.println("Image too messy");
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_FEATUREFAIL:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    case FINGERPRINT_INVALIDIMAGE:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    default:
-      Serial.println("Unknown error");
-      return p;
-  }
+int FingerprintCreateModel(){
+  return finger.createModel();
+}
 
-  Serial.println("Remove finger");
-  delay(2000);
-  p = 0;
-  while (p != FINGERPRINT_NOFINGER) {
-    p = finger.getImage();
-  }
-  Serial.print("ID "); Serial.println(id);
-  p = -1;
-  Serial.println("Place same finger again");
-  while (p != FINGERPRINT_OK) {
-    p = finger.getImage();
-    switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image taken");
-      break;
-    case FINGERPRINT_NOFINGER:
-      Serial.print(".");
-      break;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      break;
-    case FINGERPRINT_IMAGEFAIL:
-      Serial.println("Imaging error");
-      break;
-    default:
-      Serial.println("Unknown error");
-      break;
-    }
-  }
+int FingerprintStoreModel(int id){
+  return finger.storeModel(id);
+}
 
-  // OK success!
-
-  p = finger.image2Tz(2);
-  switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image converted");
-      break;
-    case FINGERPRINT_IMAGEMESS:
-      Serial.println("Image too messy");
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_FEATUREFAIL:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    case FINGERPRINT_INVALIDIMAGE:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    default:
-      Serial.println("Unknown error");
-      return p;
-  }
-
-  // OK converted!
-  Serial.print("Creating model for #");  Serial.println(id);
-
-  p = finger.createModel();
+int FingerprintSearch(){ //Ez nem az állapotot adja vissza, hanem az azonosítót
+  int p = finger.fingerSearch();
   if (p == FINGERPRINT_OK) {
-    Serial.println("Prints matched!");
-  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-    Serial.println("Communication error");
-    return p;
-  } else if (p == FINGERPRINT_ENROLLMISMATCH) {
-    Serial.println("Fingerprints did not match");
-    return p;
-  } else {
-    Serial.println("Unknown error");
-    return p;
+    return finger.fingerID;
+  }else{
+    return -1;  
   }
-
-  Serial.print("ID "); Serial.println(id);
-  p = finger.storeModel(id);
-  if (p == FINGERPRINT_OK) {
-    Serial.println("Stored!");
-  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-    Serial.println("Communication error");
-    return p;
-  } else if (p == FINGERPRINT_BADLOCATION) {
-    Serial.println("Could not store in that location");
-    return p;
-  } else if (p == FINGERPRINT_FLASHERR) {
-    Serial.println("Error writing to flash");
-    return p;
-  } else {
-    Serial.println("Unknown error");
-    return p;
-  }
-
-  return true;
 }
 
 void setup(){
@@ -264,6 +154,7 @@ void setup(){
   }
 }
 
+
 void loop(){
   //Figyeljük a bejövő soros adatforgalmat
   if (Serial.available()){
@@ -278,9 +169,59 @@ void loop(){
         Serial.println(); //Küldjünk egy sor végét is a soros kommunikáción keresztül
         tx.clear(); //Töröljük a json adatszerkezet tartalmát, mert már nincs szükségünk rá!
       }
-      if(rx["action"] == "custom_beep"){
-        CustomBeep(rx["frequency"], rx["delay"]);
+      if(rx["action"] == "fp_get_image"){
+        int p = FingerprintGetImage();
+        tx["type"] = "event"; //Megadjuk az adat típusát (event)
+        tx["event"] = "fp_done"; //Megadjuk az eseményt (card_detected)
+        tx["status"] = p; //Hozzáadjuk az adatszerkezethez az uid-t
+        serializeJson(tx, Serial); //Szerializáljuk és továbbítjuk soros kommunikáción keresztül a JSON adatszerkezetünket
+        Serial.println(); //Küldjünk egy sor végét is a soros kommunikáción keresztül
+        tx.clear(); //Töröljük a json adatszerkezet tartalmát, mert már nincs szükségünk rá!
       }
+
+      if(rx["action"] == "fp_gen_template"){
+        int p = FingerprintGenerateTemplate(rx["nr"]);
+        tx["type"] = "event"; //Megadjuk az adat típusát (event)
+        tx["event"] = "fp_done"; //Megadjuk az eseményt (card_detected)
+        tx["status"] = p; //Hozzáadjuk az adatszerkezethez az uid-t
+        serializeJson(tx, Serial); //Szerializáljuk és továbbítjuk soros kommunikáción keresztül a JSON adatszerkezetünket
+        Serial.println(); //Küldjünk egy sor végét is a soros kommunikáción keresztül
+        tx.clear(); //Töröljük a json adatszerkezet tartalmát, mert már nincs szükségünk rá!
+      }
+      
+      if(rx["action"] == "fp_create_model"){
+        int p = FingerprintCreateModel();
+        tx["type"] = "event"; //Megadjuk az adat típusát (event)
+        tx["event"] = "fp_done"; //Megadjuk az eseményt (card_detected)
+        tx["status"] = p; //Hozzáadjuk az adatszerkezethez az uid-t
+        serializeJson(tx, Serial); //Szerializáljuk és továbbítjuk soros kommunikáción keresztül a JSON adatszerkezetünket
+        Serial.println(); //Küldjünk egy sor végét is a soros kommunikáción keresztül
+        tx.clear(); //Töröljük a json adatszerkezet tartalmát, mert már nincs szükségünk rá!
+      }
+
+      if(rx["action"] == "fp_store_model"){
+        int p = FingerprintStoreModel(rx["id"]);
+        tx["type"] = "event"; //Megadjuk az adat típusát (event)
+        tx["event"] = "fp_done"; //Megadjuk az eseményt (card_detected)
+        tx["status"] = p; //Hozzáadjuk az adatszerkezethez az uid-t
+        serializeJson(tx, Serial); //Szerializáljuk és továbbítjuk soros kommunikáción keresztül a JSON adatszerkezetünket
+        Serial.println(); //Küldjünk egy sor végét is a soros kommunikáción keresztül
+        tx.clear(); //Töröljük a json adatszerkezet tartalmát, mert már nincs szükségünk rá!
+      }
+
+      if(rx["action"] == "fp_search"){
+        int p = {FingerprintSearch()};
+        tx["type"] = "event"; //Megadjuk az adat típusát (event)
+        tx["event"] = "fp_done"; //Megadjuk az eseményt
+        tx["finger"] = p; 
+        serializeJson(tx, Serial); //Szerializáljuk és továbbítjuk soros kommunikáción keresztül a JSON adatszerkezetünket
+        Serial.println(); //Küldjünk egy sor végét is a soros kommunikáción keresztül
+        tx.clear(); //Töröljük a json adatszerkezet tartalmát, mert már nincs szükségünk rá!
+      }
+
+      /*if(rx["action"] == "custom_beep"){
+        CustomBeep(rx["frequency"], rx["delay"]);
+      }*/
       if(rx["action"] == "lcd_goto"){
         LcdGoto(rx["row"], rx["column"]);
       }
